e05e1ffa4f8f2755c3f3a3f645b610d60d4984ac
Landon Cayia <landon.cayia@gmail.com>
2022-09-13 12:49:26 -0400

    Add swipe action to toggle contact status

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -11,14 +11,18 @@ class Prospect: Identifiable, Codable {
     var id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
-    var isContacted = false
+    fileprivate(set) var isContacted = false
 }
 
 @MainActor class Prospects: ObservableObject {
     @Published var people: [Prospect]
     
     init() {
-        // will be changed later
         people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
     }
 }
