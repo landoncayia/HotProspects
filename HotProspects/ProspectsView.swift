e61bcc5b638e3e999a4c1d05d4ff65d73cef33d3
Landon Cayia <landon.cayia@gmail.com>
2022-09-13 12:38:51 -0400

    Enable QR code scanning

--- HotProspects/ProspectsView.swift
+++ HotProspects/ProspectsView.swift
@@ -5,6 +5,7 @@
 //  Created by Landon Cayia on 9/12/22.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ProspectsView: View {
@@ -13,8 +14,9 @@ struct ProspectsView: View {
     }
     
     @EnvironmentObject var prospects: Prospects
+    @State private var isShowingScanner = false
     let filter: FilterType
-
+    
     var body: some View {
         NavigationView {
             List {
@@ -30,13 +32,13 @@ struct ProspectsView: View {
             .navigationTitle(title)
             .toolbar {
                 Button {
-                    let prospect = Prospect()
-                    prospect.name = "John Appleseed"
-                    prospect.emailAddress = "john.appleseed@icloud.com"
-                    prospects.people.append(prospect)
+                    isShowingScanner = true
                 } label: {
                     Label("Scan", systemImage: "qrcode.viewfinder")
                 }
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "John Appleseed\njohn.appleseed@icloud.com", completion: handleScan)
             }
         }
     }
@@ -60,6 +62,23 @@ struct ProspectsView: View {
             return prospects.people.filter { $0.isContacted }
         case .uncontacted:
             return prospects.people.filter { !$0.isContacted }
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: "\n")
+            guard details.count == 2 else { return }
+            
+            let person = Prospect()
+            person.name = details[0]
+            person.emailAddress = details[1]
+            prospects.people.append(person)
+        case .failure(let error):
+            print("Scanning failed: \(error.localizedDescription)")
         }
     }
 }
