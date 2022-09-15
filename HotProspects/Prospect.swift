dc12db4d7ab5439c6783b733794f45b9a309d19f
Landon Cayia <landon.cayia@gmail.com>
2022-09-15 10:53:42 -0400

    Switch to use document directory with JSON for data

--- HotProspects/Prospect.swift
+++ HotProspects/Prospect.swift
@@ -16,23 +16,24 @@ class Prospect: Identifiable, Codable {
 
 @MainActor class Prospects: ObservableObject {
     @Published private(set) var people: [Prospect]
-    let saveKey = "SavedData"
+//    let saveKey = "SavedData"
+    let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedProspects")
     
     init() {
-        if let data = UserDefaults.standard.data(forKey: saveKey) {
-            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
-                people = decoded
-                return
-            }
+        do {
+            let data = try Data(contentsOf: savePath)
+            people = try JSONDecoder().decode([Prospect].self, from: data)
+        } catch {
+            people = []
         }
-        
-        // no saved data!
-        people = []
     }
     
     private func save() {
-        if let encoded = try? JSONEncoder().encode(people) {
-            UserDefaults.standard.set(encoded, forKey: saveKey)
+        do {
+            let data = try JSONEncoder().encode(people)
+            try data.write(to: savePath, options: [.atomicWrite, .completeFileProtection])
+        } catch {
+            print("Unable to save data: \(error.localizedDescription)")
         }
     }
     
